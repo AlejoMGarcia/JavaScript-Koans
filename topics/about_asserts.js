48c668ec12c24862dfc68a7ead59096558220b6d
AlejoMGarcia <ale.garciaa210@gmail.com>
2021-08-26 19:31:28 -0300

    firs commit with test passing

--- topics/about_asserts.js
+++ topics/about_asserts.js
@@ -1,20 +1,14 @@
-// module("About Asserts (topics/about_asserts.js)");
 const { ok, equal } = require('assert')
-// const { __, test } = require('../support/koans')
-const koans = require('../support/koans')
-// const __ = koans.__
-// const test = koans.test
-const { __ } = koans
-const { test } = koans
+const test = require('../support/koans').test
 
 test("ok", () => {
-    assert.ok(__ === true, 'what will satisfy the ok assertion?');
+    ok(true === true, 'what will satisfy the ok assertion?');
 })
 
 test("not ok", () => {
-    assert.ok(__ === false, 'what is a false value?');
+    ok(false === false, 'what is a false value?');
 })
 
 test("ok", () => {
-    assert.equal(__, 1 + 1, 'what will satisfy the equal assertion?');
+    equal(2, 1 + 1, 'what will satisfy the equal assertion?');
 })
